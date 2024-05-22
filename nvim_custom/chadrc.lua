7626d1cecbac6f41b27712d4453997cea7a0efb2
Bruno Croci <crocidb@gmail.com>
2024-05-22 15:57:42 +0000

    added nvim mappingss for symbol search

--- nvim_custom/chadrc.lua
+++ nvim_custom/chadrc.lua
@@ -1,6 +1,11 @@
 ---@type ChadrcConfig
 local M = {}
 
+-- Keymaps
+local map = vim.keymap.set
+map("n", "<leader>ss", "<cmd> Telescope lsp_document_symbols <CR>", { desc = "Telescope Document Symbols" })
+
+-- NVChad options
 M.ui = { theme = 'vscode_dark' }
 M.plugins = "custom.plugins"
 
