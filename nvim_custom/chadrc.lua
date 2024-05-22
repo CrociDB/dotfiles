ec96121def3031875d54c13c3cb84b557206b67b
Bruno Croci <crocidb@gmail.com>
2024-05-22 16:23:29 +0000

    fixing formatter, it's not automatic anymore

--- nvim_custom/chadrc.lua
+++ nvim_custom/chadrc.lua
@@ -5,6 +5,8 @@ local M = {}
 local map = vim.keymap.set
 map("n", "<leader>ss", "<cmd> Telescope lsp_document_symbols <CR>", { desc = "Telescope Document Symbols" })
 map("n", "<leader>m", "<cmd> Telescope lsp_document_symbols ignore_symbols=variable <CR>", { desc = "Telescope Document Symbols" })
+map("n", "<leader>fm", "<cmd> FormatLock <CR>", { desc = "Format file with Formatter" })
+map("n", "<leader>fmm", "<cmd> FormatWriteLock <CR>", { desc = "Format file with Formatter and Save" })
 
 -- NVChad options
 M.ui = { theme = 'vscode_dark' }
