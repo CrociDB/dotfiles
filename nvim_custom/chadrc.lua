4fa880bbe4da1fff4f6da39455d780433be08baf
Bruno Croci <crocidb@gmail.com>
2024-05-22 16:05:16 +0000

    added telescope method/functions mapping

--- nvim_custom/chadrc.lua
+++ nvim_custom/chadrc.lua
@@ -4,6 +4,7 @@ local M = {}
 -- Keymaps
 local map = vim.keymap.set
 map("n", "<leader>ss", "<cmd> Telescope lsp_document_symbols <CR>", { desc = "Telescope Document Symbols" })
+map("n", "<leader>m", "<cmd> Telescope lsp_document_symbols ignore_symbols=variable <CR>", { desc = "Telescope Document Symbols" })
 
 -- NVChad options
 M.ui = { theme = 'vscode_dark' }
