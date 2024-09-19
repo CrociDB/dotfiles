d296b2c78b77799f563574673bd7544284d26b0e
Bruno Croci <crocidb@gmail.com>
2024-09-19 15:42:24 +0200

    removed notify messages from noice

--- nvim_custom/configs/noice.lua
+++ nvim_custom/configs/noice.lua
@@ -28,6 +28,16 @@ require("noice").setup({
     ---@type NoiceViewOptions
     opts = {}, -- merged with defaults from documentation
   },
+  messages = {
+    -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+    -- This is a current Neovim limitation.
+    enabled = true, -- enables the Noice messages UI
+    view = "mini", -- default view for messages
+    view_error = "mini", -- view for errors
+    view_warn = "mini", -- view for warnings
+    view_history = "messages", -- view for :messages
+    view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+  },
   -- you can enable a preset for easier configuration
   presets = {
     bottom_search = true, -- use a classic bottom cmdline for search
