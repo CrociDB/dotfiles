2975503f11cb1548c9166da78e7704698b270e74
Bruno Croci <crocidb@gmail.com>
2025-12-02 13:54:06 +0100

    updaing fzf default parameters

--- shell.sh
+++ shell.sh
@@ -2,3 +2,5 @@ alias vim=nvim
 
 alias xc="xclip -selection clipboard"
 alias xv="xclip -selection clipboard -o"
+
+export FZF_DEFAULT_OPTS='--height 60% --tmux center,60% --style full --preview "cat {}"'
